f726447063de562a4f21b27e6cb7a717e329d796
Jaye <joshua.hackett93@gmail.com>
2020-06-26 14:36:45 +0100

    fix build bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import Layout, { ResultsList, Welcome } from "./Layout"
 import ResultCard from "./ResultCard"
 import SearchBar from "./SearchBar"
